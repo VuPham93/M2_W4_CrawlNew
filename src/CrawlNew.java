13681531f41d1a7b474afe551f0964229e9b63c3
VuPham93 <62596412+VuPham93@users.noreply.github.com>
2020-05-19 16:32:12 +0700

    Initial commit

---
+++ src/CrawlNew.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CrawlNew {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://dantri.com.vn/the-gioi.htm");
+
+            Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
+            scanner.useDelimiter("\\Z");
+
+            String content = scanner.next();
+            content = content.replaceAll("\\n+", "");
+
+            Pattern pattern = Pattern.compile(".htm\">(.*?)</a>");
+            Matcher matcher = pattern.matcher(content);
+
+            while (matcher.find()) {
+                System.out.println(matcher.group(1));
+            }
+            scanner.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
